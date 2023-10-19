816a739eb00777cf8e20a35ae8dd307740f3b130
Martina Garcia <martinagg@myuax.com>
2023-10-19 09:41:23 +0200

    .

--- algoritmos_ordenacion/desglosando_buble.py
+++ algoritmos_ordenacion/desglosando_buble.py
@@ -1,4 +1,5 @@
 lista=[1,3,2,4]
-for i in range(len(lista)-1):
-    if lista[i]>lista[i+1]:
-        lista[i],lista[i+1]=lista[i+1],lista[i]
+for j in range(10):
+    for i in range(len(lista)-1):
+        if lista[i]>lista[i+1]:
+            lista[i],lista[i+1]=lista[i+1],lista[i]
